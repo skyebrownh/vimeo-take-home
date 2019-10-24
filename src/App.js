03fd466ab43dcc9e1bf3ad82ebfc3bf740688998
Skye Brown <skye.brown@uky.edu>
2019-10-24 16:26:38 -0400

    layout 1 html

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const App = () => {
   return (
-    <h1>Hello, Vimeo!</h1>
+    <div>
+      <div className="monsoon">
+        <h2>Monsoon III</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis expedita amet itaque nemo nesciunt aperiam blanditiis id sunt vitae ab dolore obcaecati, odit cumque voluptatem facilis ipsum maiores dolor magni veritatis eos iusto. Nulla, rerum?</p>
+        <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg" alt="Monsoon III Image"/>
+      </div>
+      <div className="beams">
+        <div className="beams-content">
+          <h2>Beams</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis ut quibusdam veniam est perferendis commodi optio quod reiciendis sapiente veritatis, nesciunt quis dolor ad natus facilis. Voluptatum molestiae architecto praesentium nesciunt exercitationem deserunt expedita quasi voluptatibus, animi fugiat, ab inventore.</p>
+        </div>
+        <img src="https://i.vimeocdn.com/video/589972810_530x315.jpg" alt="Beams Image"/>
+        <div className="move2">
+          <h2>Move 2</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut reiciendis voluptatibus pariatur aliquam maiores, quidem iste impedit repellat, quo incidunt facere labore. Porro voluptas optio earum ipsa ducimus cumque voluptatem?</p>
+        </div>
+        <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg" alt="Move 2 Image"/>
+      </div>
+    </div>
   );
 }
 
