deb525ca677b5fbaaa81c6aa83e01d39d106cec7
Skye Brown <skye.brown@uky.edu>
2019-10-24 17:29:47 -0400

    html semantics, css reset, start App css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <div className="monsoon">
+    <main>
+      <section className="monsoon">
         <h2>Monsoon III</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis expedita amet itaque nemo nesciunt aperiam blanditiis id sunt vitae ab dolore obcaecati, odit cumque voluptatem facilis ipsum maiores dolor magni veritatis eos iusto. Nulla, rerum?</p>
         <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg" alt="Monsoon III Image"/>
-      </div>
-      <div className="beams">
+      </section>
+      <section className="beams">
         <div className="beams-content">
           <h2>Beams</h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis ut quibusdam veniam est perferendis commodi optio quod reiciendis sapiente veritatis, nesciunt quis dolor ad natus facilis. Voluptatum molestiae architecto praesentium nesciunt exercitationem deserunt expedita quasi voluptatibus, animi fugiat, ab inventore.</p>
@@ -19,8 +19,11 @@ const App = () => {
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut reiciendis voluptatibus pariatur aliquam maiores, quidem iste impedit repellat, quo incidunt facere labore. Porro voluptas optio earum ipsa ducimus cumque voluptatem?</p>
         </div>
         <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg" alt="Move 2 Image"/>
-      </div>
-    </div>
+      </section>
+      {/* <div className="carousel">
+
+      </div> */}
+    </main>
   );
 }
 
